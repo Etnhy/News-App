5780c0ea6d72567a94b4db91ac20d9808ecd854d
Evhenii Mahlena <magellan7777@icloud.com>
2022-05-17 13:02:34 +0300

    Initial Commit

---
+++ NewsV2ref/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsV2ref
+//
+//  Created by Evhenii Mahlena on 17.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
